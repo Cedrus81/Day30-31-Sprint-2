a217b6815e998cc132647ab84728e89fd8471ea8
Cedrus81 <erez.eitan.97@gmail.com>
2022-10-26 12:53:20 +0300

    added photo gallery

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ var gKeywords = [
 
 function init() {
     renderKeywords()
+    renderGallery()
 }
 
 
@@ -25,7 +26,17 @@ function renderKeywords() {
     elList.innerHTML = strHTML
 }
 
-
+function renderGallery() {
+    const elGallery = document.querySelector('.main-gallery')
+    //get images
+    // for each, add inside .main-gallery
+    const images = getImages()
+    console.log(images);
+    images.forEach(image =>
+        elGallery.innerHTML += `<img class="gallery-item" src="${image.url}" alt="">`
+    )
+    // debugger
+}
 
 
 
@@ -37,9 +48,34 @@ function getKeyWords() {
     return gKeywords
 }
 
+function getImages() {
+    const images = []
+    for (let i = 0; i < 18; i++) {
+        images[i] = {
+            id: makeId(),
+            url: `./images-square/${i + 1}.jpg`,
+            keywords: [],
+        }
+    }
+    return images
+}
+
 
 // return the wanted size of the keyword on the search-bar. maximum allowed is 2.5em
 function setFontSize(value) {
     return Math.min(2.5, 1 + value * 0.1)
 }
 
+
+
+// todo: move to service - utils
+
+function makeId(length = 5) {
+    var txt = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < length; i++) {
+        txt += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return txt;
+}
